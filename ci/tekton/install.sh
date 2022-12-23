a0db6bd479178c5640a795831bca992413fb09ea
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-12-23 15:00:56 +0100

    chore: Better plance for these files

--- ci/tekton/install.sh
+++ ci/tekton/install.sh
@@ -8,7 +8,7 @@ set -eu -o pipefail
 #
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl apply --namespace=tekton-pipelines --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
+echo "kubectl --namespace tekton-pipelines port-forward svc/tekton-dashboard 9097:9097"
 kubectl apply --namespace=tekton-pipelines --filename ./tasks/git-clone.yaml
 kubectl apply --namespace=tekton-pipelines --filename ./tasks/kaniko.yaml
-kubectl create --namespace=tekton-pipelines secret docker-registry ghcr --docker-username=${GITHUB_USERNAME} --docker-password=${GITHUB_TOKEN} --docker-server="https://ghcr.io/v1"
-kubectl apply --namespace=tekton-pipelines apply tekton-ci-sa.yaml
+kubectl apply --namespace=tekton-pipelines --filename ./pipelines/ci.yaml
