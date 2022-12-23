f754860aff255c8c80f0f710a693841886da58c8
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-12-23 19:37:21 +0100

    feature: Adopt the Echo framework

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func root(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello!")
+}
 
 func main() {
-	fmt.Println("Hello!")
+	e := echo.New()
+
+	e.GET("/", root)
+
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: e,
+	}
+
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
