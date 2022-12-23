d4aa71c9d54772ed756b3cb6b20e43626d0459d1
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-12-23 15:01:29 +0100

    chore: The infrastructure stuff

---
+++ infrastructure/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+#set -x
+
+#
+# ::main::
+#
+kind create cluster --config tekton-playground-cluster.yaml
+kubectl apply  --filename ./ns/tekton-pipelines-ns.yaml
+kubectl create --namespace=tekton-pipelines secret docker-registry ghcr --docker-username=${GITHUB_USERNAME} --docker-password=${GITHUB_TOKEN} --docker-server="https://ghcr.io/v1"
+kubectl apply  --namespace=tekton-pipelines --filename ./sa/tekton-ci-sa.yaml
