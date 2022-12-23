864946831bc57d9110d6e5bb2f11a6092cfe77a9
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-12-23 18:46:16 +0100

    chore: Structure re-organization

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -7,6 +7,8 @@ set -eu -o pipefail
 # ::main::
 #
 kind create cluster --config tekton-playground-cluster.yaml
-kubectl apply  --filename ./ns/tekton-pipelines-ns.yaml
-kubectl create --namespace=tekton-pipelines secret docker-registry ghrc --docker-username=${GITHUB_USERNAME} --docker-password=${GITHUB_TOKEN} --docker-server="https://ghcr.io/v1"
-kubectl apply  --namespace=tekton-pipelines --filename ./sa/tekton-ci-sa.yaml
+kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl apply --namespace=tekton-pipelines --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
+kubectl create -n tekton-pipelines secret docker-registry ghrc --docker-username="${GITHUB_USERNAME}" --docker-password="${GITHUB_TOKEN}" --docker-server="https://ghcr.io/v1"
+kubectl apply -n tekton-pipelines --filename ./tekton-ci-sa.yaml
+kubectl apply -n tekton-pipelines --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
